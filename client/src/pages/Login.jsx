c0d9dcead3a47ffdc4dccad7704ef361ee4acfb7
Arun Dixit <arundixit2005@gmail.com>
2026-07-25 16:00:28 +0530

    Fix login API route

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await API.post("/auth/login", {
+      const res = await API.post("/api/auth/login", {
         email,
         password,
       });
